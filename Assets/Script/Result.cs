496a67216a03c0ff7d95bfd656827a459283d9bc
kcomwaki <78574875+kcomwaki@users.noreply.github.com>
2022-08-08 17:16:50 +0900

    waki_20220808

---
+++ Assets/Script/Result.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Result : MonoBehaviour
+{
+    public int Score = 23;
+
+    public SpriteRenderer spriteRenderer;
+    public Sprite[] sprite;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        //ç∂ÉNÉäÉbÉN
+        if (Input.GetMouseButtonDown(0))
+            spriteRenderer.sprite = sprite;
+    }
+}
