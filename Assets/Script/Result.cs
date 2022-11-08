0de3f017a9b4dacd35153d0b5a17c891f0108d0f
kimura <44352258+kcomcreator@users.noreply.github.com>
2022-11-08 12:34:04 +0900

    update

--- Assets/Script/Result.cs
+++ Assets/Script/Result.cs
@@ -19,18 +19,23 @@ public class Result : MonoBehaviour
         TENS=6,
         ONES=7,
         RESULT=8,
-        COUNT=9
+        COUNT=9,
+        IMAITI=10,
+        OMIGOTO=11,
+        APPARE=12
     };
 
 
     bool k1 = true;
 
-    //メモ：0～
     public int Score = 0;
+    int Result_Score = 0;
     int kaki1, kaki2, kaki3;
     GameObject[] objs = new GameObject[10];
     public Sprite[] sprites = new Sprite[13];
 
+    // 最終評価が出力されたらタップでタイトルへ
+    //メモ：0～3いまいち　4～7おみごと 7～あっぱれ
     private bool process_end_flg = false;
 
     string[] str = { 
@@ -47,7 +52,10 @@ public class Result : MonoBehaviour
         kaki2 = Score / 10;  Score %= 10;
         kaki3 = Score;
 
+        // 合計スコア表示用に計算
         Score = kaki1 + kaki2 + kaki3;
+        // 評価表示用に計算
+        Result_Score = Score;
 
         // 非アクティブ化しているUIオブジェクトを取得
         int i = 0;
@@ -58,13 +66,13 @@ public class Result : MonoBehaviour
         }
 
         // 牡蠣の個数を判別して描画設定を変更する処理
-        StartCoroutine(kaki1_num());
-        StartCoroutine(kaki2_num());
-        StartCoroutine(kaki3_num());
-        StartCoroutine(Score_num());
+        StartCoroutine(kaki1_num());// 遷移から2秒後
+        StartCoroutine(kaki2_num());// 遷移から3秒後
+        StartCoroutine(kaki3_num());// 遷移から4秒後
+        StartCoroutine(Score_num());// 遷移から5.5秒後
 
         // 総合評価スタンプを表示する処理
-        StartCoroutine(Stamp());
+        StartCoroutine(Stamp());// 遷移から7.5秒後
     }
 
     void Update()
@@ -177,6 +185,20 @@ public class Result : MonoBehaviour
     private IEnumerator Stamp()
     {
         yield return new WaitForSeconds(7.5f);
+
+        // Canvasにある結果表示用オブジェクト(result)を取得する
+        Image img_result = objs[(int)UI_OBJS.RESULT].GetComponent<Image>();
+
+        //メモ：0～3いまいち　4～7おみごと 7～あっぱれ
+        // sprites[]10いまいち 11おみごと 12あっぱれ
+        if (Result_Score >= 0 && Result_Score <= 3)
+            img_result.sprite = sprites[(int)UI_OBJS.IMAITI];
+        else if(Result_Score >= 4 && Result_Score <= 7)
+            img_result.sprite = sprites[(int)UI_OBJS.OMIGOTO];
+        else if (Result_Score >= 7)
+            img_result.sprite = sprites[(int)UI_OBJS.APPARE];
+
+        objs[(int)UI_OBJS.RESULT].SetActive(true);
 
         process_end_flg = true;
     }
